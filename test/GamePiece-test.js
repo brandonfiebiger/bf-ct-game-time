9fd367785f7c6261b08c8083f0ad30e9350e9fc8
Cody Taft <cody.taft@gmail.com>
2018-06-27 16:18:58 -0600

    Add frog, log, car, and tests

--- test/GamePiece-test.js
+++ test/GamePiece-test.js
@@ -1,4 +1,4 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
 const GamePiece = require('../lib/GamePiece.js');
 
 describe('GamePiece', function() {
@@ -6,7 +6,7 @@ describe('GamePiece', function() {
   it('should have properties', function() {
     const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0', 1)
 
-    assert.deepEqual(gamePiece, {
+    expect(gamePiece).to.equal(gamePiece, {
       x: 50,
       y: 50,
       height: 10,
