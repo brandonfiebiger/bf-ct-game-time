d75c98ce2ba594adc8722a3d98407b34b024d942
Cody Taft <cody.taft@gmail.com>
2018-06-28 11:24:51 -0600

    add moveLeft and moveRight tests

--- test/GamePiece-test.js
+++ test/GamePiece-test.js
@@ -17,5 +17,40 @@ describe('GamePiece', function() {
     })
   })
 
+  it('should have a function called draw', function() {
+    const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0', 1)
+    
+    expect(gamePiece.draw).to.be.a('function');
+  })
+
+  it('should have a function called moveRight', function() {
+    const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0', 1)
+
+    expect(gamePiece.moveRight).to.be.a('function');
+  })
+
+  it('has a function called moveRight that should reset x to 0 when at the end of the canvas', function() {
+    const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0', 1)
+    for (var i = 0; i < 1300; i++) {
+      gamePiece.moveRight();
+    }
+    expect(gamePiece.x === 0)
+  })
+
+  it('should have a function called moveLeft', function() {
+    const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0', 1)
+
+    expect(gamePiece.moveLeft).to.be.a('function');
+  })
+
+  it('has a function called moveLeft that should reset x to 700 when at the end of the canvas', function() {
+    const gamePiece = new GamePiece(50, 50, 10, 10, 'rgb(250, 0, 0', 1)
+    for (var i = 0; i < 1300; i++) {
+      gamePiece.moveLeft();
+    }
+    expect(gamePiece.x === 700)
+  })
+
+
 })
 
