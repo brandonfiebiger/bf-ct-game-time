a34fd09d539b2272a5974e515bb073b8c613f19a
Cody Taft <cody.taft@gmail.com>
2018-07-01 19:08:46 -0600

    Add and update frog tests

--- test/water.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require('chai');
-const Water = require('../lib/Water.js');
-
-describe('Water', function() {
-  it('should be an object', function() {
-    const water = new Water();
-    expect(water).to.be.an('object')
-  });
-});
