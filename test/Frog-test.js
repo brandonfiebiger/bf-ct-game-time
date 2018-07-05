b9547fa4e2a7563407ef5b6764bf156d6735852f
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-07-04 19:03:04 -0600

    Make trophies into frogs

--- test/Frog-test.js
+++ test/Frog-test.js
@@ -17,20 +17,20 @@ describe('Frog', function() {
   it('should hop left', function() {
     const frog = new Frog();
     frog.hopLeft();
-    expect(frog.x).to.equal(312);
+    expect(frog.x).to.equal(293);
   });
 
   it('should hop right', function() {
     const frog = new Frog();
     frog.hopRight();
-    expect(frog.x).to.equal(388)
+    expect(frog.x).to.equal(407)
   });
 
   it('should hop up', function() {
     const scoreAndLives = new ScoreAndLives;
     const frog = new Frog();
     frog.hopUp(scoreAndLives);
-    expect(frog.y).to.equal(601);
+    expect(frog.y).to.equal(630);
   });
 
   it('should be able to die', function() {
@@ -40,7 +40,7 @@ describe('Frog', function() {
 
     expect(scoreAndLives.lives).to.equal(2);
     expect(frog.x).to.equal(350);
-    expect(frog.y).to.equal(639);
+    expect(frog.y).to.equal(680);
   })
   
 })
